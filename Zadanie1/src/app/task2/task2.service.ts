a2653c62449a032ab256a9011fcca5dd36ffe272
CYBERDOM2\addzi1 <adam.dziomdziora@cybercom.com>
2018-03-14 15:01:32 +0100

    TEST COMMIT

--- Zadanie1/src/app/task2/task2.service.ts
+++ Zadanie1/src/app/task2/task2.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
 import {forEach} from '@angular/router/src/utils/collection';
+import {Task} from 'protractor/built/taskScheduler';
+require('yasmij');
 
+declare var YASMIJ: any;
 
 @Injectable()
 export class Task2Service {
@@ -17,6 +20,7 @@ export class Task2Service {
   //   [2, 1, 0]
   // ];
 
+  private modulo = 0;
 
   private data: Array<number[]> = [
     [1, 1, 1, 2, 2],
@@ -39,7 +43,8 @@ export class Task2Service {
   }
 
 
-  private static reduceMatrix(data: Array<number[]>): Array<number[]> {
+  private static reduceMatrix(dataToReduce: Array<number[]>): Array<number[]> {
+    let data = JSON.parse(JSON.stringify(dataToReduce));
     let rowsToReduce: Set<number[]> = new Set();
     let columnIndexesToReduce: Set<number> = new Set();
 
@@ -84,11 +89,6 @@ export class Task2Service {
               shouldBeReduced = true;
             }
             if (k === data.length - 1 && shouldBeReduced) {
-
-              // take the column
-              let column: number[] = data.map((value) => {
-                return value[i];
-              });
               columnIndexesToReduce.add(i);
             }
           }
@@ -123,7 +123,7 @@ export class Task2Service {
     for (let row of data) {
       minArray.push(Math.min(...row));
     }
-    console.log(minArray);
+
     let maxMin = Math.max(...minArray);
 
     // from each column take max
@@ -136,19 +136,52 @@ export class Task2Service {
       maxArray.push(Math.max(...column));
     }
 
-    console.log(maxArray);
     let minMax = Math.min(...maxArray);
 
     return maxMin === minMax;
   }
 
+  private static findSmallestValueInData(data: Array<number[]>): number {
+    let minValueInData = data[0][0];
+    for (let row of data) {
+      let tmp = Math.min(...row);
+      if (tmp < minValueInData) {
+        minValueInData = tmp;
+      }
+    }
+    return minValueInData;
+  }
+
+  private static addModuloOfSmallestValueIfSmallerThanZero(dataToAdd: Array<number[]>): { data: Array<number[]>, moduloAdded: number } {
+    let data = JSON.parse(JSON.stringify(dataToAdd));
+    let minValueInData = Task2Service.findSmallestValueInData(data);
+    if (minValueInData < 0) {
+      minValueInData = Math.abs(minValueInData);
+      for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length; j++) {
+          data[i][j] += minValueInData;
+        }
+      }
+    }
+    return {data: data, moduloAdded: minValueInData};
+  }
+
+
   public resolve(data: Array<number[]>) {
     let isSaddlePoint = Task2Service.isSaddlePoint(data);
-    alert('isSaddlePoint: ' + isSaddlePoint);
     if (!isSaddlePoint) {
       data = Task2Service.reduceMatrix(data);
+      let dataWithModulo: { data: Array<number[]>, moduloAdded: number } = Task2Service.addModuloOfSmallestValueIfSmallerThanZero(data);
+      this.setData = dataWithModulo.data;
+      this.modulo = dataWithModulo.moduloAdded;
+      let input = {
+        type: "maximize",
+        objective: "",
+        constraints: []
+      };
+
+      let output = YASMIJ.solve(input);
     }
-    return data;
   }
 
 }
